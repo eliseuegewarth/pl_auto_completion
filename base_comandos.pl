792c84f2adcf8319163d80c2723d78f5c9fa0c27
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-04-13 14:02:07 -0300

    Atualizando a base de dados
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- base_comandos.pl
+++ base_comandos.pl
@@ -6,7 +6,10 @@ comando("pip").
 comando("python").
 comando("python3").
 flag("git", "--help").
+subComando("git", "help").
+subComando(X) :- subComando(_, X).
+
 % flag(X,Y):- comando(X), flag(X,Y); split_string(X)
 
-ehComando(Comando) :- comando(Comando).
+ehComando(Comando) :- comando(Comando), Y is (Comando),!, write(Y); subComando(Comando), Y is (Comando), !, write(Y).
 flags(Comando) :- flag(Comando,Flag), write(Flag).
